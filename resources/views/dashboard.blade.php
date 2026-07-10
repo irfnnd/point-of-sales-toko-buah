fd07bfcfff9e5fcce205f189c99215189562a215
M. Irfandi Atarza <lobuoku@gmail.com>
2026-07-11 04:09:35 +0700

    update route to return dashboard view and add dashboard blade template

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard Saya')
+
+@section('content_header')
+    <h1>Selamat Datang di AdminLTE</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <p>Berhasil menginstall AdminLTE di Laravel 13!</p>
+        </div>
+    </div>
+@stop
